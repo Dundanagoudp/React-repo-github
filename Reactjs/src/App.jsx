43ed25bc6f4499a6a9679f08e13a8dd5b7aae83c
chetanv18~ <chetan@gully2global.com>
2024-09-15 11:46:43 +0530

    changes

--- Reactjs/src/App.jsx
+++ Reactjs/src/App.jsx
@@ -1,23 +1,17 @@
-// import NetflixSeries from "./components/NetflixSeries";
-// import Profile from "./components/Profile";
+
 import "./components/Netflix.css";
 import "./components/EV.css";
-// import "./components/Netflix.module.css"; // moudle imported 
-// import {EventHandling} from "./components/EventHandling";
-// import { EventProps } from "./components/EventProps";
-// import { EventPropagation } from "./components/EventPropagation";
+
 import { State } from "./components/hooks/State";
+import { DerivedState } from "./components/hooks/DerivedState";
 export const App = () => {
      return (
           <section className="container">
-                    {/* <h1 className="card-heading">list the best netflix series movie</h1> */}
-     {/* <NetflixSeries/>; */}
-     {/* <EventHandling/> */}
-     {/* <EventProps/> */}
-     <State/>
-     {/* <EventPropagation/> */}
+     
+     {/* <State/> */}
+
+     <DerivedState/>
           </section>
-     // return<Profile/>;
      );
 };
 
