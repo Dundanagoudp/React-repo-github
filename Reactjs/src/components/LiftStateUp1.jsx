cfe16c4a9b3467370bac79e75e6cb5bd0d8b1150
chetanv18~ <chetan@gully2global.com>
2024-09-16 08:09:58 +0530

    new

---
+++ Reactjs/src/components/LiftStateUp1.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const LiftingState = () => {
+    const [inputValue, setInputValue] = useState("");  // Initialize with an empty string to avoid uncontrolled input warnings
+
+    return (
+        <>
+            <InputComponent inputValue={inputValue} setInputValue={setInputValue} />
+            <DisplayComponent inputValue={inputValue} />
+        </>
+    );
+};
+
+const InputComponent = ({ inputValue, setInputValue }) => {
+    return (
+        <>
+            <input
+                type="text"
+                placeholder="Enter your name"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+        </>
+    );
+};
+
+const DisplayComponent = ({ inputValue }) => {
+    return (
+        <p>The current input value is: {inputValue}</p>
+    );
+};
