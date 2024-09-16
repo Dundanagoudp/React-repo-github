a69fa78a997f3eaa143712f948a1dc3b3e0d9c45
chetanv18~ <chetan@gully2global.com>
2024-09-16 14:38:31 +0530

    new

--- Reactjs/src/projects/ToggleSwtich/ToggleSwtich.jsx
+++ Reactjs/src/projects/ToggleSwtich/ToggleSwtich.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import "./ToggleSwtich.css"
+import { IoIosSwitch } from "react-icons/io";
+import { FaUserSecret } from "react-icons/fa";
+
 
 export const ToggleSwtich=()=>{
 
@@ -10,10 +13,15 @@ export const ToggleSwtich=()=>{
       }
 const backg={backgroundColor:isOn ? "#4caf50" : "#f44336"};
       return (
+            <>
+            <h1 style={{color:"#000",textAlign:"center"}}>Toggle switch <IoIosSwitch style={{color:"#ff0000",textAlign:"center"}} /><FaUserSecret />
+
+            </h1>
       <div className="toggle-switch" style={backg} onClick={handletoggleSwitch} >
             <div className={`switch ${isOn ? "on" : "off"}`}>
                   <span className="switch-state">{isOn ? "ON" : "OFF"}</span>
             </div>
       </div>
+      </>
 );
 };
